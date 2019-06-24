94fcf6674664d806159ec3253ce9f6052b73f5e0
Viki.B <viki.b92@gmail.com>
2019-06-24 23:33:48 +0530

    Added views and http requests

--- profiles_rest_api/src/profiles_api/views.py
+++ profiles_rest_api/src/profiles_api/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from . import serializers
 
 # Create your views here.
 class HelloAPIView(APIView):
     """Hello world for APIView testing"""
 
+    serializers_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
         an_apiview = ['get', 'post', 'patch', 'put', 'delete']
         return Response({'message': 'Hello', 'APi_list': an_apiview})
 
+    def post(self, request):
+        """Post request with name"""
+        serializer = self.serializers_class(data = request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            msg = 'Hello ' + name
+            return Response({'messgae': msg})
+        else:
+            return Response(serializer.errors,
+                            status.HTTP_400_BAD_REQUEST
+                    )
--- profiles_rest_api/src/profiles_project/urls.py
+++ profiles_rest_api/src/profiles_project/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import include
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', )
+    path('api/', include('profiles_api.urls')),
 ]
