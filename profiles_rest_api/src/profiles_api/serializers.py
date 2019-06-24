17b059a432016777ea8562dba2434a0e0e23119f
Viki.B <viki.b92@gmail.com>
2019-06-24 23:58:07 +0530

    Added venv

--- profiles_rest_api/src/profiles_api/serializers.py
+++ profiles_rest_api/src/profiles_api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
 class HelloSerializer(serializers.Serializer):
-    """Serializes a name field for the API view"""
+    """Serializes a name field for the API views"""
 
     name = serializers.CharField(max_length=10)
